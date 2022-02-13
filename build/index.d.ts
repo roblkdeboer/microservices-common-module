367fdcfe6536aca53ba2ccab2002733fadc09045
Rob de Boer <roblkdeboer@gmail.com>
2022-02-13 15:48:44 +0800

    Updates

--- build/index.d.ts
+++ build/index.d.ts
@@ -13,4 +13,6 @@ export * from './events/templates/base-publisher';
 export * from './events/subjects';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
-export * from './types/order-status';
+export * from './events/types/order-status';
+export * from './events/order-cancelled-event';
+export * from './events/order-created-event';
